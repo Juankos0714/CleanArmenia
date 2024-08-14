d84350a35970a28cc6b90e4c34ae71ae27ace9c5
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2024-08-14 07:07:48 -0500

    Create tipos de funciones.js

---
+++ Arle-ADSO-2994283-main/tipos de funciones.js
@@ -0,0 +1,27 @@
+let x =3
+function sumadorDosNumeros(){//funciones sin paramentros y sin retorno
+    let a=3
+    let b=4
+    let suma = a+b
+    console.log(suma)
+}
+function sumarDosNumeros2(numeros1, numeros2){//funciones con paramentros y sin retorno
+    let suma = numeros + numeros2
+    console.log((suma))
+}
+sumarDosNumeros2(2,3)//argumentos
+sumarDosNumeros2(3,5)
+
+//sin parametros con retorno
+function sumadorDosNumeros3(){
+    let numero1 = 2
+    let numero2 = 3
+    return numero1 + numero2
+}
+
+function sumarDosNumeros4(numero1, numero2){
+    let numero1 = 2
+    let numero2 = 3
+    return numero1 + numero2
+}
+
