399d5276d01da9df12b7d585d5963f7f65cbb46e
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2024-08-14 07:56:26 -0500

    Update index.js

--- Arle-ADSO-2994283-main/Func/Ejercicio  (4)/index.js
+++ Arle-ADSO-2994283-main/Func/Ejercicio  (4)/index.js
@@ -1,7 +1,10 @@
-let numero = Number(prompt("Ingrese un  numero aleatorio"))
+function IdentifyPar(numero){
+numero = Number(prompt("Ingrese un  numero aleatorio"))
 
 if(numero % 2  === 0){
     alert("Su numero es  par")
 }else{
     alert("Su numero es impar")
 }
+}
+IdentifyPar()
