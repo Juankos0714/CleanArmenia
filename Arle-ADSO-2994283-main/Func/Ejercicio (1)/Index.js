3adece6dd9b4efb7b57c2ade612e1abac272888c
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2024-08-14 07:53:36 -0500

    Update Index.js

--- Arle-ADSO-2994283-main/Func/Ejercicio (1)/Index.js
+++ Arle-ADSO-2994283-main/Func/Ejercicio (1)/Index.js
@@ -1,8 +1,11 @@
-let empleado = prompt("Ingrese su nombre")
-let horas = Number(prompt("Ingresa el numero de horas que has trabajado"))
-let salario = 0
+function calcularSalario(empleado, horas, salario){
+     empleado = prompt("Ingrese su nombre")
+     horas = Number(prompt("Ingresa el numero de horas que has trabajado"))
+     salario = 0
 if(horas < 10){
     salario=(horas * 30000)
 }else salario=(horas * 33000)
 
 alert(`Señor ${empleado} el numero de horas es ${horas} y Su salario equivale a: ${salario}`)
+}
+calcularSalario()
