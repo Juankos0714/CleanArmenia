459abef8d78f4a35909acad67a2c122c0f664a8d
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2024-08-14 08:46:52 -0500

    Update index.js

--- Arle-ADSO-2994283-main/Func/Ejercicio  (8)/index.js
+++ Arle-ADSO-2994283-main/Func/Ejercicio  (8)/index.js
@@ -1,8 +1,9 @@
-let mensualidad1 = 15000
-let mensualidad2 = 35000
-let mensualidad3 = 86000
-let valorTotal = 0
-let tiempo = Number(prompt("Ingresa la cantidad de meses que deseas estar en el gimnasio, si solo deseas probar nuestro servicio por 15 dias ingresa la opcion 0"))
+function suscripcionGimnasio(mensualidad1, mensualidad2, mensualidad3, valorTotal, tiempo){
+ mensualidad1 = 15000
+ mensualidad2 = 35000
+ mensualidad3 = 86000
+ valorTotal = 0
+ tiempo = Number(prompt("Ingresa la cantidad de meses que deseas estar en el gimnasio, si solo deseas probar nuestro servicio por 15 dias ingresa la opcion 0"))
 
 if(tiempo < 1){
     valorTotal += mensualidad1
@@ -14,3 +15,5 @@ if(tiempo < 1){
     valorTotal += trimestresCompletos * mensualidad3
     valorTotal += ((tiempo % 3)*mensualidad2)
 }alert(`El valor total a pagar es ${valorTotal}`)
+}
+suscripcionGimnasio()
