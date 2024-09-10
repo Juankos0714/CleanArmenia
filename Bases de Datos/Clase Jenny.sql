6ab255a578ced1eef79ba8436688fcbc0fa9df2d
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2024-09-10 07:29:51 -0500

    Add files via upload

---
+++ Bases de Datos/Clase Jenny.sql
@@ -0,0 +1,12 @@
+-- crear la base de datos
+create database adso283;
+-- usar la bse de datos
+use adso283;
+/* 
+Consultar la version y el dia actual
+*/
+select version(), current_date();
+-- mostrar a base datos
+show databases;
+drop database adso283;
+-- eliminar base de datos
---
+++ Bases de Datos/practica.sql
@@ -0,0 +1,11 @@
+create database SENA;
+use SENA;
+create table ADSO(
+nombre varchar(30),
+phone int null,
+fecha_de_ingreso date,
+edad int null
+
+);
+show databases;
+show tables;
